04886eebda4a93e516aac3062871abfe74b21eec
manduul0804 <manduul0804@gmail.com>
2023-04-16 08:43:33 -0500

    init

--- 11. Container with most water/ContainerWithMostWater.py
+++ 11. Container with most water/ContainerWithMostWater.py
@@ -1,1 +1,8 @@
 height = [1,8,6,2,5,4,8,3,7]
+
+if (len(height) == 0):
+    print("The list is empty")
+elif (len(height) == 2):
+    size = height[0] if height[0] <= height[1] else height[1]
+for i in height:
+    print(i)
